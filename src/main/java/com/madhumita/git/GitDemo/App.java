2b4095d33600ca87ad507b1170a4a84ff2ccc95a
Madhumeeta bhatia <madhumeetabhatia@gmail.com>
2025-02-02 12:31:37 +0530

    My First commit

---
+++ src/main/java/com/madhumita/git/GitDemo/App.java
@@ -0,0 +1,19 @@
+package com.madhumita.git.GitDemo;
+
+
+public class App 
+{  
+	public void addData(String data)
+	{
+	 	
+	 if(data.equals(Constant.Data_Type))      //second commit
+	{
+		System.out.println("Welcome");
+	}	
+	}
+   
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/madhumita/git/GitDemo/Constant.java
@@ -0,0 +1,6 @@
+package com.madhumita.git.GitDemo;
+
+public class Constant {
+ public static String Data_Type = "Madhumita";
+ 
+}
